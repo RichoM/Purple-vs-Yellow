918a749267bb19d1638eab0fbb064bc877cc47e9
RichoM <richi.moran@gmail.com>
2021-09-05 09:29:48 -0300

    Changed projectiles to work with generic physics bodies and increased range.

--- Projectile.gd
+++ Projectile.gd
@@ -10,10 +10,8 @@ var attractors_in_range = []
 
 func _process(delta):
 	#apply_attractor(get_global_mouse_position(), 10, delta)
-	for area in attractors_in_range:
-		var attractor = area as Attractor
-		if attractor:
-			apply_attractor(attractor.position, attractor.strength, delta)
+	for attractor in attractors_in_range:
+		apply_attractor(attractor.position, attractor.scale.x, delta)
 		
 	line.points[1] = velocity
 	position += velocity * delta
@@ -25,8 +23,10 @@ func apply_attractor(pos: Vector2, strength: float, delta):
 	dir *= clamp(radius - length, 0, radius) * strength * 0.5
 	velocity += dir * delta
 
-func _on_range_area_entered(area):
-	attractors_in_range.append(area)
 
-func _on_range_area_exited(area):
-	attractors_in_range.erase(area)
+func _on_range_body_entered(body):
+	attractors_in_range.append(body)
+
+
+func _on_range_body_exited(body):
+	attractors_in_range.erase(body)
