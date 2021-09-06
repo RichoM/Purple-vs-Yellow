cde05278526e11a58a53c0a23d9b7235977e13c4
RichoM <richi.moran@gmail.com>
2021-09-06 12:01:22 -0300

    Added a "ta da!" sound effect and a winner label

--- game.gd
+++ game.gd
@@ -1,20 +1,35 @@
 extends Node2D
 
+var game_over = false
+
 func _on_player0_tree_exited():
+	if game_over: return
+	game_over = true
 	call_deferred("winner", "p1")
 
 func _on_player1_tree_exited():
+	if game_over: return
+	game_over = true
 	call_deferred("winner", "p0")
 
 func winner(winner):
+	var root = get_tree().get_root()
+
+	# Remove the current level
+	var level = get_tree().get_current_scene()
+	root.remove_child(level)
+	level.call_deferred("free")
+
 	var end_scene = load("res://end.tscn").instance()
 	var p0 = end_scene.get_node("p0")
 	var p1 = end_scene.get_node("p1")
 	if winner == "p0":
 		p0.play("alive")
 		p1.play("dead")
+		end_scene.get_node("winner/purple").visible = true
 	else:
 		p1.play("alive")
 		p0.play("dead")
+		end_scene.get_node("winner/yellow").visible = true
 	
-	add_child(end_scene)
+	root.add_child(end_scene)
