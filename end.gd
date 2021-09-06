cde3c974a6c76e4706c5464e06e218310d9594b6
RichoM <richi.moran@gmail.com>
2021-09-06 11:41:23 -0300

    A few very minor visual improvements

--- end.gd
+++ end.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_released("ui_accept"):
-		get_tree().change_scene("res://player_test.tscn")
+func _on_play_button_pressed():
+	get_tree().change_scene("res://player_test.tscn")
