920dc3772c6d360d4ee97e4052adff86b367a989
RichoM <richi.moran@gmail.com>
2021-12-01 20:12:13 -0300

    Added a global mute action

--- globals.gd
+++ globals.gd
@@ -3,3 +3,7 @@ extends Node
 var level = 1
 var player = null
 var server_url = "ws://042c-186-152-147-30.sa.ngrok.io"
+
+func _process(delta):
+	if Input.is_action_just_released("mute"):
+		AudioServer.set_bus_mute(0, not AudioServer.is_bus_mute(0))
