2b0de1f868ae1852e4061baeafb4664bfb1b6026
RichoM <richi.moran@gmail.com>
2021-12-06 17:56:37 -0300

    Improving visuals for the lobby screens

--- join_game.gd
+++ join_game.gd
@@ -3,5 +3,6 @@ extends Node2D
 onready var lobby = $lobby
 
 func _on_join_button_pressed():
-	lobby.game_id = $game_id.text
+	$GUI/join_button.disabled = true
+	lobby.game_id = $GUI/game_id.text
 	lobby.start()
--- new_game.gd
+++ new_game.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+onready var game_id = $GUI/game_id
+onready var label = $GUI/label
 
-func _on_lobby_joined(game_id):
-	$game_id.text = Client.lobby
+func _on_lobby_joined(lobby_id):
+	$timer.stop()
+	game_id.text = Client.lobby
+	label.text = "Game code:"
+
+func _on_timer_timeout():
+	var count = game_id.text.count(".") % 3
+	game_id.clear()
+	for i in 3:
+		game_id.text += "." if i <= count else " "
