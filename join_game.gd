4ead745c33840a87f9df6cd5acebea2550f79922
RichoM <richi.moran@gmail.com>
2021-12-06 21:32:04 -0300

    Changed game_id font to be able to distinguish 0 from O

--- join_game.gd
+++ join_game.gd
@@ -1,8 +1,22 @@
 extends Node2D
 
 onready var lobby = $lobby
+onready var game_id = $GUI/game_id
+onready var join_button = $GUI/join_button
 
 func _on_join_button_pressed():
-	$GUI/join_button.disabled = true
-	lobby.game_id = $GUI/game_id.text
+	join()
+
+func _on_game_id_text_entered(new_text):
+	join()
+
+func join():
+	join_button.disabled = true
+	lobby.game_id = game_id.text
 	lobby.start()
+
+func _on_game_id_text_changed(new_text):
+	var pos = game_id.caret_position
+	game_id.text = new_text.to_upper()
+	game_id.caret_position = pos
+	
