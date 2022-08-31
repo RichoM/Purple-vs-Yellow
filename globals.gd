39fece76c74ba9ec403419a68d955a58e5669843
RichoM <richi.moran@gmail.com>
2022-08-31 18:37:35 -0300

    Changed how the server_url is initialized

--- globals.gd
+++ globals.gd
@@ -5,8 +5,24 @@ enum {LOCAL_MULTIPLAYER, ONLINE_MULTIPLAYER}
 var timestamp_begin = 0
 var level = 1
 var player = null
-var server_url = "wss://fathomless-badlands-10412.herokuapp.com"
+var server_url = null
 var mode = LOCAL_MULTIPLAYER
+
+func _ready():
+	#server_url = "ws://localhost:3000"
+	fetch_server_url()
+	
+func fetch_server_url():
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, "_on_server_url_ready")
+	http_request.request("https://richom.github.io/sum.txt")
+
+func _on_server_url_ready(result, response_code, headers, body):
+	if response_code != 200: return
+	var data = body.get_string_from_utf8()
+	server_url = "ws" + data.strip_edges().trim_prefix("http").trim_suffix("/")
+	print(server_url)
 
 func _process(delta):
 	if Input.is_action_just_released("mute"):
