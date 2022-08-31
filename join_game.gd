dcfa7ce07c9e284793276b6fb9f70b2790c0e063
RichoM <richi.moran@gmail.com>
2022-08-31 18:38:47 -0300

    Fixed bug in the join_game script and increased timeout

--- join_game.gd
+++ join_game.gd
@@ -18,7 +18,7 @@ func _process(delta):
 		game_id.caret_position = pos
 		
 		# Enable join button if not empty
-		join_button.disabled = game_id.text.empty()
+		join_button.disabled = game_id.text.strip_edges().empty()
 		
 		previous_game_id = game_id.text
 
@@ -29,14 +29,20 @@ func _on_game_id_text_entered(new_text):
 	join()
 
 func join():
-	if game_id.text.empty(): return
+	if error_panel.visible: return
+	if join_button.disabled: return
+	
+	var id = game_id.text.strip_edges()
+	if id.empty(): return
 	
 	join_button.disabled = true
 	game_id.editable = false
-	lobby.game_id = game_id.text
+	lobby.game_id = id
 	lobby.start()
 	
-	yield(get_tree().create_timer(5), "timeout")
+	# If, for some reason, the connection cannot be made we wait 15 seconds
+	# before giving up
+	yield(get_tree().create_timer(15), "timeout")
 	lobby.stop()
 
 func _on_back_button_pressed():
